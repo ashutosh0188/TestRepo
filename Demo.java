34984d7649146498ef8853576346c7b9075ab0e1
ashutosh0188 <ashutosh0188@bitbucket.org>
2020-05-07 03:53:48 +0530

    added print sum method

--- Demo.java
+++ Demo.java
@@ -3,5 +3,10 @@ public class Demo
   public static void main(String[] args)
   {
     System.out.println("Nice to have this class");
+    printSum(5,8);
+  }
+  public static void printSum(int a, int b)
+  {
+    System.out.println(a+b);
   }
 }
